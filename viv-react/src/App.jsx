08c3bae5e02dacf4cba278074ce9b4edb2bea5e6
William Moore <w.moore@dundee.ac.uk>
2022-11-23 22:43:49 +0000

    Fix errors in viv-react example

--- viv-react/src/App.jsx
+++ viv-react/src/App.jsx
@@ -1,67 +1,47 @@
+/// app.js
 import React from 'react';
-import './App.css';
-import { openArray, HTTPStore } from 'zarr';
-import { ImageLayer, ZarrLoader } from '@hms-dbmi/viv';
-import DeckGL from 'deck.gl'
-import { OrthographicView } from '@deck.gl/core'
+import DeckGL from '@deck.gl/react';
 
-function App() {
+import {OrthographicView} from '@deck.gl/core';
+import { MultiscaleImageLayer } from '@vivjs/layers';
 
-  const vpWidth = 800;
-  const vpHeight = 500;
+import {loadOmeroMultiscales, open} from "./util";
+
+
+// DeckGL react component
+export default function App() {
+  let config = {
+    source:"https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.3/idr0079A/9836998.zarr"
+  }
 
   const [layers, setLayers] = React.useState([]);
-  // pan top-left of image to top-left of viewport. 0, 0 puts top-left of image to centre
-  // zoom of -1 is 50%
-  const [viewState, setViewState] = React.useState({ target: [vpWidth/2, vpHeight/2, 0], zoom: 0 });
-
+  
   React.useEffect(() => {
 
     const fn = async function() {
 
-      let url = 'https://minio-dev.openmicroscopy.org/idr/idr0002-heriche-condensation/plate1_1_013/422_no_T/422.zarr/0/F/1/Field_1/';
-      const data = await openArray({ store: new HTTPStore(url), path: '0' });
-      // const { chunks, shape } = data;
-      const dimensions = [0, 1, 2, 'y', 'x'].map((field) => ({ field }));
-      const loader = new ZarrLoader({ data, dimensions });
+      const node = await open(config.source);
+      
+      const attrs = await node.attrs.asObject();
+      console.log('attrs', attrs);
+      let layerData = await loadOmeroMultiscales(config, node, attrs);
+      console.log("layerData", layerData);
 
-      const layerProps = {
-        id: 'abc123',
-        loader,  // ZarrLoader
-        // source,
-        loaderSelection: [[0, 0, 0, 0, 0], [0, 1, 0, 0, 0]],
-        colorValues: [[255, 0, 0], [0, 255, 0]],
-        sliderValues: [[174, 1692], [345, 3480]],
-        // contrastLimits: [[174, 1692], [345, 3480]],
-        channelIsOn: [true, true],
-        // opacity,
-        // colormap
-      }
-      setLayers([new ImageLayer(layerProps)]);
+      setLayers([new MultiscaleImageLayer(layerData)]);
     }
 
     fn();
 
   }, []);
 
-  console.log('viewState', viewState.target, viewState.zoom);
+  const INITIAL_VIEW_STATE = {
+    zoom: -1,
+    bearing: 0,
+  };
 
-  return (
-    <div className="App">
-      Image:
-      <div style={{ marginLeft: 'auto', marginRight: 'auto', postion: 'relative', width: 800, height: 500, border: 'solid #bbb 1px'}}>
-        <DeckGL
-          layers={layers}
-          viewState={viewState}
-          views={new OrthographicView({ controller: true, id: 'ortho' })}
-          width={800}
-          height={500}
-          style={{ position: 'relative', background: '#333'}}
-          onViewStateChange={(e) => setViewState(e.viewState)}
-        />
-      </div>
-    </div>
-  );
+  return <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      views={[new OrthographicView({ id: 'ortho', controller: true })]}
+      layers={layers} />;
 }
-
-export default App;
